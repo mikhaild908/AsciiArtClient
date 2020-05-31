3389ee8277a115c04b305ba8352294551ffcd18d
Mike Dumlao <mikhaild908@gmail.com>
2020-05-31 00:18:48 -0700

    First commit

---
+++ Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+open System.Drawing
+open ImageToAsciiArt
+
+[<EntryPoint>]
+let main argv = 
+    let bitmap = AsciiArt.DrawText("MIKE")
+    let chars = AsciiArt.ConvertToAscii(bitmap)
+
+    for h in [0 .. bitmap.Height - 1] do
+        for w in [0 .. bitmap.Width - 1] do
+            Console.Write(chars.[w, h]);
+        Console.WriteLine() 
+
+    //Console.ReadLine() |> ignore
+    0
